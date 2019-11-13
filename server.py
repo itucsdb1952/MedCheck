3a4566602a28847d44378dea21b7c9f58dcff320
furkanx007 <guvencf16@itu.edu.tr>
2019-11-13 17:30:22 +0300

    hospitals can be added

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask import request
 
 from views import *
@@ -27,12 +27,52 @@ def admin_page():
 
 @app.route("/hospitals")
 def hospitals_page():
-    return render_template('admin_hospitals.html')
+    cities = get_cities()
+    cities = [city[0] for city in cities]
+
+    districts = get_districts()
+    districts = [district[0] for district in districts]
+    return render_template('admin_hospitals.html', cities=cities, districts=districts)
+
+
+@app.route("/add_hospital",methods=['POST'])
+def add_hospital_page():
+    hospital_name = request.form.get('hospital_name')
+    city = request.form.get('city_select')
+    district = request.form.get('district_select')
+    park = request.form.get('park')
+    if park == 'on':
+        park = True
+    else:
+        park = False
+    handicapped = request.form.get('handicapped')
+    if handicapped == 'on':
+        handicapped = True
+    else:
+        handicapped = False
+    response = add_hospital(hospital_name, city, district, park, handicapped)
+
+    return response
 
 
 @app.route("/doctors")
 def doctors_page():
     return render_template('admin_doctors.html')
+
+
+@app.route("/get_districts", methods=['POST', 'GET'])
+def get_districts_ajax_page(city_name):
+    print("GET DISTRICT AJAX")
+    districts = get_districts_ajax(city_name)
+    districts = [district[0] for district in districts]
+    result = jsonify(districts)
+    print(result)
+    return result
+
+
+@app.route("/get_hebe", methods=['POST', 'GET'])
+def hebe_ajax(city_name):
+    return jsonify({'data': 'Loooooo'})
 
 
 if __name__ == "__main__":
--- views.py
+++ views.py
@@ -37,7 +37,7 @@ def get_hospitals(limit: int = 100, city: str = None, district: str = None) -> l
                 else:  # fetch all hospitals
                     statement += "WHERE hospital.address = place.id "
 
-                statement += "LIMIT {}".format(limit)
+                statement += "ORDER BY name LIMIT {}".format(limit)
 
                 cursor.execute(statement)
                 record = cursor.fetchall()
@@ -45,6 +45,118 @@ def get_hospitals(limit: int = 100, city: str = None, district: str = None) -> l
 
     except (Exception, dbapi2.Error) as error:
         print("Error while connecting to PostgreSQL: {}".format(error), file=sys.stderr)
+
+    finally:
+        # closing database connection.
+        if connection:
+            cursor.close()
+            connection.close()
+
+
+def get_cities() -> list:
+    """
+
+    :return:
+    """
+
+    try:
+        with dbapi2.connect(**DSN) as connection:  # TODO: LOCAL
+            # with dbapi2.connect(os.getenv("DATABASE_URL")) as connection:  # TODO: HOST
+            with connection.cursor() as cursor:
+                statement = "SELECT DISTINCT city FROM place ORDER BY city"
+
+                cursor.execute(statement)
+                record = cursor.fetchall()
+                return record
+
+    except (Exception, dbapi2.Error) as error:
+        print("Error while connecting to PostgreSQL: {}".format(error), file=sys.stderr)
+
+    finally:
+        # closing database connection.
+        if connection:
+            cursor.close()
+            connection.close()
+
+
+def get_districts() -> list:
+    """
+
+    :return:
+    """
+
+    try:
+        with dbapi2.connect(**DSN) as connection:  # TODO: LOCAL
+            # with dbapi2.connect(os.getenv("DATABASE_URL")) as connection:  # TODO: HOST
+            with connection.cursor() as cursor:
+                statement = "SELECT DISTINCT district FROM place ORDER BY district"
+
+                cursor.execute(statement)
+                record = cursor.fetchall()
+                return record
+
+    except (Exception, dbapi2.Error) as error:
+        print("Error while connecting to PostgreSQL: {}".format(error), file=sys.stderr)
+
+    finally:
+        # closing database connection.
+        if connection:
+            cursor.close()
+            connection.close()
+
+
+def get_districts_ajax(city: str = None) -> list:
+    """
+
+    :param city:
+    :return:
+    """
+
+    try:
+        with dbapi2.connect(**DSN) as connection:  # TODO: LOCAL
+            # with dbapi2.connect(os.getenv("DATABASE_URL")) as connection:  # TODO: HOST
+            with connection.cursor() as cursor:
+                if city:
+                    statement = "SELECT district FROM place WHERE city = '{}'".format(city)
+
+                    cursor.execute(statement)
+                    record = cursor.fetchall()
+                    return record
+                else:
+                    return [('il seciniz',)]
+
+    except (Exception, dbapi2.Error) as error:
+        print("Error while connecting to PostgreSQL: {}".format(error), file=sys.stderr)
+
+    finally:
+        # closing database connection.
+        if connection:
+            cursor.close()
+            connection.close()
+
+
+def add_hospital(name: str = None, city: str = None, district: str = None, park: bool = False,
+                 handicapped: bool = True) -> str:
+    try:
+        with dbapi2.connect(**DSN) as connection:  # TODO: LOCAL
+            # with dbapi2.connect(os.getenv("DATABASE_URL")) as connection:  # TODO: HOST
+            with connection.cursor() as cursor:
+                address_statement = "SELECT ID FROM place WHERE (city = '{}' AND district = '{}');".format(city,
+                                                                                                          district)
+                cursor.execute(address_statement)
+                address = cursor.fetchone()[0]
+
+                add_statement = "INSERT INTO hospital(name, address,park,handicapped) " \
+                                "VALUES('{}',{},'{}','{}');".format(name, address, park, handicapped)
+
+                cursor.execute(add_statement)
+                print("hebe")
+                return "Succesfull"
+
+
+    except (Exception, dbapi2.Error) as error:
+        print("Error while connecting to PostgreSQL: {}".format(error), file=sys.stderr)
+        return str(error)
 
     finally:
         # closing database connection.
